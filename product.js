24511fa2384cb7ca9f5bf5424a0233f2d2523194
Trac620 <whitneybenson2000@gmail>
2021-03-12 01:08:32 +0100

    Commit 1

---
+++ product.js
@@ -0,0 +1,83 @@
+let productdata=[
+    {
+        id: 1001,
+        subject: "Mathematics",
+        location: "Malta",
+        price: 500,
+        space: 5,
+        image: "images/product-maths.jpg"
+    },
+    {
+        id: 1002,
+        subject: "Physics",
+        location: "Lagos",
+        price: 300,
+        space: 5,
+        image: "images/product-phys.jpg"
+    },
+    {
+        id: 1003,
+        subject: "Music",
+        location: "Senegal",
+        price: 500,
+        space: 5,
+        image: "images/product-music.jpg"
+    },
+    {
+        id: 1004,
+        subject: "Business Intelligence",
+        location: "Maldives",
+        price: 400,
+        space: 5,
+        image: "images/product-bi.jpg"
+    },
+    {
+        id: 1005,
+        subject: "Engish",
+        location: "Minna",
+        price: 500,
+        space: 5,
+        image: "images/product-eng.jpg"
+    },
+    {
+        id: 1006,
+        subject: "Social Studies",
+        location: "Santorini",
+        price: 600,
+        space: 5,
+        image: "images/product-sos.jpg"
+    },
+    {
+        id: 1007,
+        subject: "Computer Science",
+        location: "Valetta",
+        price: 700,
+        space: 5,
+        image: "images/product-comp.jpg"
+    },
+    {
+        id: 1008,
+        subject: "Sex Education",
+        location: "India",
+        price: 800,
+        space: 5,
+        image: "images/product-sexedu.jpg"
+    },
+    {
+        id: 1009,
+        subject: "Civic Education",
+        location: "Lagos",
+        price: 900,
+        space: 5,
+        image: "images/product-civic.jpg"
+    },
+    {
+        id: 1010,
+        subject: "Literature",
+        location: "Isreal",
+        price: 200,
+        space: 5,
+        image: "images/product-lit-.jpg"
+    }
+]
+//export default lessondata 
